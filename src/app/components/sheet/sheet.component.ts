74b686d600785f17626f859901bfa567339d2593
ameerthehacker <ameerjhanprof@gmail.com>
2018-02-28 03:07:45 +0530

    Update values from the table to the database reactively :fire:

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 
+import { AuthService } from "../../services/auth/auth.service";
 @Component({
   selector: "tm-sheet",
   templateUrl: "./sheet.component.html",
@@ -9,10 +10,13 @@ export class SheetComponent implements OnInit {
   @Input() sheetDetails: any;
   editable = {};
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
+  parseInt(str) {
+    return new Number(str);
+  }
   onCurrentYearEditClick(evt, houseId, taxId) {
     evt.preventDefault();
     this.makeCurrentYearEditable(houseId, taxId, true);
@@ -21,10 +25,12 @@ export class SheetComponent implements OnInit {
     if (evt.keyCode == 13) {
       evt.preventDefault();
       this.makeCurrentYearEditable(houseId, taxId, false);
+      this.updateCurrentYearAmount(sheetId, houseId, taxId, evt.target.value);
     }
   }
   onCurrentYearBlur(evt, sheetId, houseId, taxId) {
     this.makeCurrentYearEditable(houseId, taxId, false);
+    this.updateCurrentYearAmount(sheetId, houseId, taxId, evt.target.value);
   }
   onPaidAmountEditClick(evt, houseId, taxId) {
     evt.preventDefault();
@@ -34,19 +40,56 @@ export class SheetComponent implements OnInit {
     if (evt.keyCode == 13) {
       evt.preventDefault();
       this.makePaidAmountEditable(houseId, taxId, false);
+      this.updatePaidAmount(sheetId, houseId, taxId, evt.target.value);
     }
   }
   onPaidAmountBlur(evt, sheetId, houseId, taxId) {
     this.makePaidAmountEditable(houseId, taxId, false);
+    this.updatePaidAmount(sheetId, houseId, taxId, evt.target.value);
   }
   makeCurrentYearEditable(houseId, taxId, editable) {
     this.sheetDetails.balanceSheet[houseId].taxes[
       taxId
     ].currentYearEditable = editable;
+    // Update if the editable is being removed
+    if (!editable) {
+    }
   }
   makePaidAmountEditable(houseId, taxId, editable) {
     this.sheetDetails.balanceSheet[houseId].taxes[
       taxId
     ].paidAmountEditable = editable;
+  }
+  updateCurrentYearAmount(sheetId, houseId, taxId, amount) {
+    this.authService
+      .put(`sheets/amount/${sheetId}`, {
+        houseId: houseId,
+        taxId: taxId,
+        amount: amount
+      })
+      .then(() => {
+        this.sheetDetails.balanceSheet[houseId].taxes[
+          taxId
+        ].currentYear = amount;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  updatePaidAmount(sheetId, houseId, taxId, amount) {
+    this.authService
+      .put(`sheets/paid/${sheetId}`, {
+        houseId: houseId,
+        taxId: taxId,
+        amount: amount
+      })
+      .then(() => {
+        this.sheetDetails.balanceSheet[houseId].taxes[
+          taxId
+        ].paidAmount = amount;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
